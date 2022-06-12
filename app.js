23d4680e7e065d3f03ac8b7fee88bbaf6fd04c88
luis <luis_4664@hotmail.com>
2022-06-11 20:44:41 -0700

    curso

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+//conexion a base de datos 
+
+const mongoose = require('mongoose'); 
+
+const user = 'luis-vete'; 
+const password = 'm9zKKZjAni6zn0YA'; 
+const dbname = 'veteriana'
+const uri = `mongodb+srv://${user}:${password}@cluster3.zm6yq.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}
+    ) 
+    .then(() => console.log('base de datos conectada'))
+    .catch(e => console.log(e))
+
+// motor de plantillas
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+
+app.use(express.static(__dirname + "/public"));
+
+// Rutas Web
+app.use('/', require('./router/RutasWeb'));
+app.use('/mascotas', require('./router/Mascotas'));
+
+app.use((req, res, next) => {
+    res.status(404).render("404", {
+        titulo: "404",
+        descripcion: "Título del sitio web"
+    })
+})
+
+
+app.listen(port, () => {
+    console.log('servidor a su servicio en el puerto', port)
+})
