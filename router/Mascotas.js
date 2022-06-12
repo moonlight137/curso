a6781ab6257975d2f03484660f6d516e53c6c75a
luis <luis_4664@hotmail.com>
2022-06-11 22:46:48 -0700

    se agregó editar

--- router/Mascotas.js
+++ router/Mascotas.js
@@ -1,13 +1,64 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render("mascotas", {
-        arrayMascotas: [
-            {id: 'jdjdjdfas', nombre: 'rex', descripcion: 'rex descripcion'},
-            {id: 'jdjdjapss', nombre: 'chanchan', descripcion: 'chanchan descripcion'},
-        ]
-    })
+const Mascota = require('../models/mascota')
+
+router.get('/', async (req, res) => {
+    try { 
+
+        const arrayMascotasDB = await Mascota.find()
+        console.log(arrayMascotasDB)
+
+        res.render("mascotas", {
+            arrayMascotas: arrayMascotasDB
+
+        })
+
+    }catch(error){
+        console.log(error)
+    }
+
+
+    
+})
+
+router.get('/crear', (req,res)=> {
+    res.render('crear')
+})
+
+router.post('/', async (req,res) => {
+    const body = req.body
+    try{
+       
+        await Mascota.create(body)
+        
+        res.redirect('/mascotas')
+
+    }catch(error){
+        console.log(error)
+    }
+})
+
+router.get('/:id', async(req,res) =>  {
+
+    const id = req.params.id
+
+    try{
+
+        const mascotaDB = await Mascota.findOne({_id: id})
+        
+        res.render('detalle', { 
+            mascota: mascotaDB,
+            error: false
+        })
+
+    } catch(error){
+        res.render('detalle', { 
+           
+            error: true,
+            mensaje: 'No se encuentra el id seleccionado'
+        })
+    }
 })
 
 module.exports = router;
